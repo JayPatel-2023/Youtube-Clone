1d4fcef77ba101a305017f36ae6ba5c491fe760f
JayPatel-2023 <mr.jaypatel2002@gmail.com>
2024-08-25 06:41:11 +0530

    - add code for logging in production

--- src/app.js
+++ src/app.js
@@ -1,6 +1,8 @@
 import express from "express";
 import cors from "cors";
 import cookieParser from "cookie-parser";
+import logger from "./utils/logger.js";
+import morgan from "morgan";
 
 const app = express();
 
@@ -23,8 +25,29 @@ app.use(express.static("public"));
 // to access(CURD) users secure cookies of their browser in our server
 app.use(cookieParser());
 
+// for logging format
+const morganFormat = ":method :url :status :response-time ms";
+
+// middleware for logging
+app.use(
+  morgan(morganFormat, {
+    stream: {
+      write: (message) => {
+        const logObject = {
+          method: message.split(" ")[0],
+          url: message.split(" ")[1],
+          status: message.split(" ")[2],
+          responseTime: message.split(" ")[3],
+        };
+        logger.info(JSON.stringify(logObject));
+      },
+    },
+  })
+);
+
 // for testing purpose
 app.get("/", (req, res) => {
+  logger.info("This is an info message");
   res.send("This is a backend server");
 });
 
@@ -39,13 +62,13 @@ import userRouter from "./routes/user.routes.js";
 import videoRouter from "./routes/video.routes.js";
 
 // routes declaration
-app.use("/api/v1/comments", commentRouter)
-app.use("/api/v1/dashboard", dashboardRouter)
-app.use("/api/v1/likes", likeRouter)
-app.use("/api/v1/playlist", playlistRouter)
-app.use("/api/v1/subscriptions", subscriptionRouter)
-app.use("/api/v1/tweets", tweetRouter)
+app.use("/api/v1/comments", commentRouter);
+app.use("/api/v1/dashboard", dashboardRouter);
+app.use("/api/v1/likes", likeRouter);
+app.use("/api/v1/playlist", playlistRouter);
+app.use("/api/v1/subscriptions", subscriptionRouter);
+app.use("/api/v1/tweets", tweetRouter);
 app.use("/api/v1/users", userRouter);
-app.use("/api/v1/videos", videoRouter)
+app.use("/api/v1/videos", videoRouter);
 
 export { app };
